d21ab27798217e74b1e5a33adafb9573d883535c
Andrew Halligan <ahalligan@nearlyfreehosting.com>
2012-02-05 18:30:49 -0500

    added router

--- src/application.js
+++ src/application.js
@@ -1,19 +1,46 @@
-(function() {
+var datalogger = {
 
-    var DataLogger = this.DataLogger || {};
+    Router: Backbone.Router.extend({
+        routes: {
+            '': 'index',
+            'settings': 'settings',
+            'logs': 'logs',
+            'add-template': 'add_template'
+        },
 
-    DataLogger.init = function() {
-        //Initialize app
+        index: function () {},
+
+        settings: function() {
+            $.mobile.changePage($('#settings'), { transition: "none" });
+            $('.ui-btn-active').removeClass('ui-btn-active');
+        },  
+
+        logs: function() {
+            $.mobile.changePage($('#logs'), { transition: "none" });
+            $('.ui-btn-active').removeClass('ui-btn-active');
+        },
+
+        add_template: function() {
+            $.mobile.changePage($('#add-template'), { transition: "slide" });
+            $('.ui-btn-active').removeClass('ui-btn-active');
+        }
+    }),
+
+    init: function() {
+        var router = new datalogger.Router();
+        Backbone.history.start();
     }
-})(this);
 
-$(function(){
-    DataLogger.init();
+};
+
+
+$(function() {
+    datalogger.init();
 });
 
-/*$(document).bind("mobileinit", function(){
+$(document).bind('mobileinit', function(){      
     $.mobile.ajaxEnabled = false;
     $.mobile.linkBindingEnabled = false;
     $.mobile.hashListeningEnabled = false;
-});/*
+});
 
